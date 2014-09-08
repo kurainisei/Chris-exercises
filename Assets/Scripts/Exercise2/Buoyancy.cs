cc0593261cb92c7f4993d0cc437a290ea4957cb2
Federico Fasce <federico@urustar.net>
2014-09-08 14:12:39 +0200

    Started Exercise #2. Right now, basic floating for the boat works, as well as a simple control system (Z/X to activate left/right engine). It almost works as I'd like to, but I'm looking for a more physical behavior: the boat should roll and pitch when curving.

---
+++ Assets/Scripts/Exercise2/Buoyancy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buoyancy : MonoBehaviour {
+	public float waterLevel;
+	public float floatingForce;
+
+	private Transform _floatingLine;
+
+
+	// Use this for initialization
+	void Start () {
+		_floatingLine = transform.Find ("floatingLine");
+	}
+	
+	void FixedUpdate () {
+		if (_floatingLine.position.y<waterLevel)
+			rigidbody.AddForce(Vector3.up*floatingForce, ForceMode.Acceleration);
+	}
+}
---
+++ Assets/Scripts/Exercise2/EngineControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineControl : MonoBehaviour {
+	public bool isOn=false;
+	public string operateKey;
+	public float engineSpeed;
+	private Transform _boat;
+	private Rigidbody _boatRB;
+	// Use this for initialization
+	void Start () {
+		_boat = transform.parent;
+		_boatRB = _boat.rigidbody;
+	}
+
+	void Update()
+	{
+		if (Input.GetButton(operateKey))
+			isOn=true;
+		else
+			isOn=false;
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (isOn)
+		{
+			_boatRB.AddForceAtPosition(transform.forward*engineSpeed,transform.position);
+		}
+	}
+}
